9b6d5689726bf11edacb7cda876f8b606c9906e2
NtimYeboah <lylegendary@gmail.com>
2018-01-06 14:53:17 +0000

    Initial commit

---
+++ app/Console/Commands/RegisterAdminCommand.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\User;
+use Illuminate\Console\Command;
+
+class RegisterAdminCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'register:admin';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Register super admin';
+
+    /**
+     * User model.
+     *
+     * @var object
+     */
+    private $user;
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct(User $user)
+    {
+        parent::__construct();
+
+        $this->user = $user;
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $details = $this->getDetails();
+
+        $admin = $this->user->create($details);
+
+        $this->display($admin);
+    }
+
+    /**
+     * Ask for admin details.
+     *
+     * @return array
+     */
+    private function getDetails() : array
+    {
+        $details['name'] = $this->ask('Name');
+        $details['email'] = $this->ask('Email');
+        $details['password'] = $this->secret('Password');
+        $details['confirm_password'] = $this->secret('Confirm password');
+
+        while (! ($this->isRequiredLength($details['password'])
+        && $this->isMatch($details['password'], $details['confirm_password']))) {
+            if (! $this->isRequiredLength($details['password'])) {
+                $this->error('Password must be more that six characters');
+            }
+
+            if (! $this->isMatch($details['password'], $details['confirm_password'])) {
+                $this->error('Password and Confirm password do not match');
+            }
+
+            $details['password'] = $this->secret('Password');
+            $details['confirm_password'] = $this->secret('Confirm password');
+        }
+
+        return $details;
+    }
+
+    /**
+     * Display created admin.
+     *
+     * @param array $admin
+     * @return void
+     */
+    private function display(User $admin) : void
+    {
+        $headers = ['Name', 'Email', 'Super admin'];
+
+        $fields = [
+            'Name' => $admin->name,
+            'email' => $admin->email,
+            'admin' => $admin->isSuperAdmin()
+        ];
+
+        $this->info('Super admin created');
+        $this->table($headers, [$fields]);
+    }
+
+    /**
+     * Check if password and confirm password matches.
+     *
+     * @param string $password
+     * @param string $confirmPassword
+     * @return bool
+     */
+    private function isMatch(string $password, string $confirmPassword) : bool
+    {
+        return $password === $confirmPassword;
+    }
+
+    /**
+     * Checks if password is longer than six characters.
+     *
+     * @param string $password
+     * @return bool
+     */
+    private function isRequiredLength(string $password) : bool
+    {
+        return strlen($password) > 6;
+    }
+}
---
+++ app/User.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App;
+
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class User extends Authenticatable
+{
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    /**
+     * Checks if user is a super admin
+     *
+     * @return boolean
+     */
+    public function isSuperAdmin() : bool
+    {
+        return (bool) $this->is_super_admin;
+    }
+
+    /**
+     * Create admin.
+     *
+     * @param array $details
+     * @return array
+     */
+    public function create(array $details) : self
+    {
+        $user = new self($details);
+
+        if (! $this->superAdminExists()) {
+            $user->is_super_admin = 1;
+        }
+
+        $user->save();
+
+        return $user;
+    }
+
+    /**
+     * Checks if super admin exists.
+     *
+     * @return object|null
+     */
+    public function superAdminExists()
+    {
+        return self::where('is_super_admin', 1)->first();
+    }
+}
